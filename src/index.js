93a3066c1a6f1bd29170f18688dfc3e65daa406d
Thomas Flack <thomas.flack@skybettingandgaming.com>
2018-11-25 22:32:52 +0000

    add cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import 'dotenv/config'
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 
-app.get('/', (req, res) => res.status(200).send('Hello'))
+app.get('/', (req, res) => res.status(200).send({text: 'hello'}))
 
 app.listen(process.env.PORT || 9001)
